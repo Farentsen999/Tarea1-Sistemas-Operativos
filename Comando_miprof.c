61cc07f18980c2c810a2edda32a7c10aef71b79a
farentsen <franciscoarentsenalister@gmail.com>
2025-09-23 13:32:00 -0300

    Idea para el Comando especial 'miprof'

---
+++ Comando_miprof.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void ejecutar_miProof(char **arr) {
+    //segundo argumento maxtime y ultimo argumento archivo
+    //primero determina si el primer caracter del segundo argumento es un numero
+    //si lo es guardar ese numero como limite y si no fijar el limite de tiempo en infinito
+    //ejecutar el comando con execvp y el tiempo si tarda de mas detener el proceso hijo
+    //en el caso exitoso determinar metricas y almacenar en archivo
+
+    if(strcmp(arr[1][0], "maxtiempo") == 0) {
+    }
+
+
+}
---
+++ Comando_miprof.h
@@ -0,0 +1,4 @@
+#ifndef MIPROOF_H
+#define MIPROOF_H
+void ejecutar_miProof(char **args);
+#endif
