627c0bdaaa0e60a5255e62c76e3f34a5ef741242
SebastianNovaOne <sebaxnovax@gmail.com>
2025-09-26 23:11:02 -0300

    Merge desde miprof a main

--- parser.c
+++ parser.c
@@ -8,42 +8,47 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
 
-void commandParser(char *inputTokens[], char *args[]) {
-    int arg_count = 0;
+int detectorCantidadPipes(char *inputTokens[], char **comandos[]) {
     int pipes = 0;
-    char *command_line[MAX_STRINGS][MAX_ARGS];
-    char **comandos[MAX_STRINGS]; 
+    int arg_count = 0;
+    static char *command_line[MAX_STRINGS][MAX_ARGS];  // static para que exista despues de que la funcion haga return
 
     for (int i = 0; inputTokens[i] != NULL; i++) {
         if (strcmp(inputTokens[i], "|") == 0) {
             command_line[pipes][arg_count] = NULL;
-            comandos[pipes] = command_line[pipes]; 
+            comandos[pipes] = command_line[pipes];
             pipes++;
             arg_count = 0;
             continue;
-        } else{
+        } else {
             command_line[pipes][arg_count++] = inputTokens[i];
         }
     }
-    
-    command_line[pipes][arg_count] = NULL;
-    comandos[pipes] = command_line[pipes]; 
 
-    if (pipes != 0)
-    {
+    command_line[pipes][arg_count] = NULL;
+    comandos[pipes] = command_line[pipes];
+
+    return pipes;
+}
+
+void commandParser(char *inputTokens[], char *args[]) {
+    char **comandos[MAX_STRINGS];
+    int pipes = detectorCantidadPipes(inputTokens, comandos);
+
+    if (pipes != 0) {
         exec_with_pipes(comandos, pipes);
     } else {            // SIN PIPES
-        for (int i = 0; inputTokens[i] != NULL; i++) {
-            args[i] = inputTokens[i];
+        int arg_count = 0;
+        for (; inputTokens[arg_count] != NULL; arg_count++) {
+            args[arg_count] = inputTokens[arg_count];
         }
         args[arg_count] = NULL;
     }
 
-    
     /*/
     *inputFile = NULL;
     *outputFile = NULL;
@@ -53,10 +58,10 @@ void commandParser(char *inputTokens[], char *args[]) {
         /*/
         if (strcmp(inputTokens[i], "<") == 0 && inputTokens[i + 1] != NULL) {
             *inputFile = inputTokens[++i];
-        } 
+        }
         else if (strcmp(inputTokens[i], ">") == 0 && inputTokens[i + 1] != NULL) {
             *outputFile = inputTokens[++i];
-        } 
+        }
         else {
             args[argCount++] = inputTokens[i];
             if (argCount >= MAX_ARGS - 1) break;
@@ -64,13 +69,13 @@ void commandParser(char *inputTokens[], char *args[]) {
         /*/
 
     /*/
-    args[argCount] = NULL; 
+    args[argCount] = NULL;
     if (*outputFile != NULL) {
         pid_t pid = fork();
         if (pid == 0) { // hijo
             int fd = open(*outputFile, O_WRONLY | O_CREAT | O_TRUNC, 0664);
             if (fd == -1) {
-                perror("Error al crear el archivo"); 
+                perror("Error al crear el archivo");
                 exit(1);
             }
             dup2(fd, STDOUT_FILENO); // redirigir stdout al archivo
@@ -78,11 +83,11 @@ void commandParser(char *inputTokens[], char *args[]) {
             ejecutar_comando(args, *inputFile, *outputFile);   // ejecutar comando, se puede llamar a la función?
             exit(1); // al momento de finalizar el proceso se hace "dup_free", y el resto se imprime en la terminal
         } else if (pid > 0) { // padre
-            waitpid(pid, NULL, 0); 
+            waitpid(pid, NULL, 0);
         } else {
             perror("fork falló");
             exit(1);
-        }  
+        }
     }
     /*/
 }
--- parser.h
+++ parser.h
@@ -2,5 +2,6 @@
 #define PARSER_H
 
 void commandParser(char *inputTokens[], char *args[]);
+int detectorCantidadPipes(char *inputTokens[], char **comandos[]);
 
 #endif
